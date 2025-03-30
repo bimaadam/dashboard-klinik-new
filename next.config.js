32dd5a6ffb535780bf32069ddbcd250e39fc9caf
bimaadam <rinbim.dev@gmail.com>
2025-03-30 21:22:17 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true, // Biar build tetap lanjut walau ada error
+  },
 };
 
 module.exports = nextConfig;
